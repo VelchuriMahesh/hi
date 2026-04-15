6b6214eadfe81e3bdc156e13162268886fb68c03
Velchuri Mahesh <velchurimahesh77@gmail.com>
2026-04-15 14:08:20 +0530

    Delete hi-main directory

--- hi-main/server/routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-
-import { Router } from 'express';
-import { getGoogleReviews } from '../controllers/reviewController.js';
-
-const router = Router();
-
-router.get('/', getGoogleReviews);
-
-export default router;
